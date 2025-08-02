ca8fefb9a68ec00f6d15420a25fcd52f206edaa5
Allen Lin <allenl5879@gmail.com>
2025-08-02 17:11:19 -0400

    Scene transitions

--- main.gd
+++ main.gd
@@ -6,6 +6,9 @@ func game_over():
 	$HUD.show_game_over()
 	#pass
 	
+func start_game():
+	new_game()
+	
 func new_game():
 	coins = 0
 	$Player.start($StartPosition.position)
--- menu.gd
+++ menu.gd
@@ -1,5 +1,4 @@
 extends CanvasLayer
 
-
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://area1.tscn")
